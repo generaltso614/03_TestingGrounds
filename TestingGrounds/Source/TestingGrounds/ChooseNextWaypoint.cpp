3671d571a839260d0ebd0f8c163e8473a09fe5ea
YOURDESKTOPNICK\ntimpano <mellowyellow614@gmail.com>
2018-07-18 11:02:45 +0900

    Guard moves to new waypoints, converted to C++

--- TestingGrounds/Source/TestingGrounds/ChooseNextWaypoint.cpp
+++ TestingGrounds/Source/TestingGrounds/ChooseNextWaypoint.cpp
@@ -1,7 +1,39 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "ChooseNextWaypoint.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "Engine/World.h"
+#include "AIController.h"
+#include "TestingGrounds/TP_ThirdPerson/PatrollingGuard.h"
 
+EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent & OwnerComp, uint8 * NodeMemory)
+{
+	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
+	Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
+	
+	// set the patrolling guard ref and protect pointers 
+	auto AIController = OwnerComp.GetAIOwner();
+	auto ControlledPawn = AIController->GetPawn();
+	PatrollingGuardRef = Cast<APatrollingGuard>(ControlledPawn);
+	if (!ensure(PatrollingGuardRef)) { UE_LOG(LogTemp, Warning, TEXT("No patroller")) }
 
+	SetNextWayPoint(BlackboardComp);
+
+	// finish execute 
+	return EBTNodeResult::Succeeded;
+}
+
+void UChooseNextWaypoint::SetNextWayPoint(UBlackboardComponent* BlackboardComp)
+{
+		auto Points = PatrollingGuardRef->GetPoints();
+		auto length = PatrollingGuardRef->GetPoints().Num();
+		
+		
+		BlackboardComp->SetValueAsObject(WaypointKey.SelectedKeyName, Points[Index]);
+		UE_LOG(LogTemp, Warning, TEXT("Index %i"), Index)
+
+		auto NextIndex = (Index + 1) % length;
+		BlackboardComp->SetValueAsInt(IndexKey.SelectedKeyName, NextIndex);
+}
 
 
--- TestingGrounds/Source/TestingGrounds/ChooseNextWaypoint.h
+++ TestingGrounds/Source/TestingGrounds/ChooseNextWaypoint.h
@@ -2,6 +2,7 @@
 
 #pragma once
 
+
 #include "CoreMinimal.h"
 #include "BehaviorTree/BTTaskNode.h"
 #include "ChooseNextWaypoint.generated.h"
@@ -9,12 +10,27 @@
 /**
  * 
  */
+
+class APatrollingGuard;
+
 UCLASS()
 class TESTINGGROUNDS_API UChooseNextWaypoint : public UBTTaskNode
 {
 	GENERATED_BODY()
-	
-	
-	
-	
+
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+
+protected:
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	struct FBlackboardKeySelector IndexKey; // he uses struct in front of fblackboard
+
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	FBlackboardKeySelector WaypointKey;
+
+	APatrollingGuard* PatrollingGuardRef = nullptr;
+
+	int32 Index; 
+
+	void SetNextWayPoint(UBlackboardComponent* BlackboardComp);
+
 };
